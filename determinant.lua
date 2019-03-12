7d429ee1ac369cd6685f1f01cfef3c1cf4b47a6a
Beddy1101 <masasuke674@gmail.com>
2019-03-12 16:53:12 +0900

    complete n order

--- determinant.lua
+++ determinant.lua
@@ -14,15 +14,14 @@ det_fact.hor = io.read()
 ---Square matrix
 squdet = {}
 
-io.write("n order?")
-squdet.n = io.read()
-
-
-
-
-
-
-
-
-
-
+io.write("n order? : ")
+squdet.n = io.read("*n")
+while true do
+    if (squdet.n == 0 or squdet.n == 1) then
+        print("Don't write 0 or 1 ")
+        io.write("one more time : ")
+        squdet.n = io.read("*n")
+    else break
+    end
+end
+print(squdet.n)

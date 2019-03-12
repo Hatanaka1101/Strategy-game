a68ad490035f7a85c953af4f460ff0a1d77ca365
Beddy1101 <masasuke674@gmail.com>
2019-03-12 14:04:23 +0900

    First commit

---
+++ determinant.lua
@@ -0,0 +1,28 @@
+--[[ Calculation for deffirent det
+
+print("How long will it be determinant?")
+det_fact = {}
+
+io.write("Var : ")
+det_fact.var = io.read()
+
+io.write("Hor : ")
+det_fact.hor = io.read()
+
+]]-- 
+
+---Square matrix
+squdet = {}
+
+io.write("n order?")
+squdet.n = io.read()
+
+
+
+
+
+
+
+
+
+

fc9a2f1aa741f995ce781b1054de122fd8dd1a91
Beddy1101 <masasuke674@gmail.com>
2019-03-13 00:50:53 +0900

    complete input

--- determinant.lua
+++ determinant.lua
@@ -32,9 +32,16 @@ end
 ]]
 
 element = {}
-io.write("Element of the determinant : ")
 i = 1
-while i < squdet.n*squdet.n do
+local var = 0
+local hor = 0
+while i < squdet.n * squdet.n do
     i = i + 1
-    element[i] = io.read("*n")
+    for var = var + 1, squdet.n do
+        for hor = hor + 1, squdet.n do
+            io.write("Element of the determinant n", var, hor," : ")
+            element[i] = io.read("*n")
+        end
+    end
+    break
 end
